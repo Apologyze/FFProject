094a2e6cb8fd99442b8b9707995625332f683548
HUSSON Floran <floranhusson@gmail.com>
2015-11-04 20:59:08 +0100

    Premier commit du projet

---
+++ ckeditor/config.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.language = 'fr';
+	config.uiColor = '#ff2e2e';
+	config.height = 300;
+	config.toolbarCanCollapse = true;
+};
+
+CKEDITOR.config.contentsCss = '/MyWork/css/ckeditor_style.css' ;
